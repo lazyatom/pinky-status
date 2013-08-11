44a11c8f6cfc9f69046e6e0bd1b396d72b86acf8
James Adam <james@lazyatom.com>
2013-08-11 12:30:54 -0500

    Hooks don't work, weird.

--- app.rb
+++ app.rb
@@ -8,7 +8,16 @@ DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/pinky-s
 class Event
   include DataMapper::Resource
   property :id, Serial
+  property :source, Enum[:manual, :twine], default: :twine
+  property :status, Enum[:in, :out]
   property :created_at, DateTime
+
+  def self.next_status
+    most_recent = all(order: [:created_at.desc]).first
+    if most_recent
+      most_recent.status == :in ? :out : :in
+    end
+  end
 end
 
 DataMapper.finalize
@@ -21,5 +30,6 @@ get "/" do
 end
 
 post "/event" do
-  Event.create!(created_at: Time.now)
+  Event.create!({created_at: Time.now, status: Event.next_status}.merge(params[:event]))
+  redirect "/"
 end
