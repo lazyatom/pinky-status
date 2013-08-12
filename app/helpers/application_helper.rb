6452c1240c95faadedf77463a34e1c9937b13692
James Adam <james@lazyatom.com>
2013-08-11 23:11:40 -0500

    Style tweaks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def admin?
     params[:admin].present?
   end
+
+  def arrow(status)
+    if status == "in"
+      content_tag(:i, "", class: %w(icon icon-arrow-left))
+    else
+      content_tag(:i, "", class: %w(icon icon-arrow-right))
+    end
+  end
 end
